a4dcbf8acf453b61384228622e35160a70fd760c
Miguel Mejia <miguelmejiamontes@me.com>
2016-04-15 19:43:47 -0400

    begin parsing url. Currently just returning path as raw data

--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	"net/http"
 )
 
+type timestamp struct {
+	Human string
+	Unix  string
+}
+
 func main() {
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":8080", nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello!"))
+	w.Write([]byte(r.URL.Path))
 }
