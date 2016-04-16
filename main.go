581f8ce92baf73315dcbc03bc07eac6c3b097a1f
Miguel Mejia <miguelmejiamontes@me.com>
2016-04-15 20:24:55 -0400

    now returning object correctly. Time encoding/translation broken.

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type timestamp struct {
-	Human string
-	Unix  string
+	Unix  string `json:"unix"`
+	Human string `json:"natural"`
 }
 
 func main() {
@@ -15,5 +19,23 @@ func main() {
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(r.URL.Path))
+	path := strings.TrimPrefix(r.URL.Path, "/")
+
+	parsedString := parseTimestring(path)
+
+	obj, _ := json.Marshal(parsedString)
+	w.Write([]byte(obj))
+}
+
+func parseTimestring(timestring string) timestamp {
+	humanForm := "January 01, 2016"
+	if _, err := strconv.Atoi(timestring); err != nil {
+		t, _ := time.Parse(humanForm, timestring)
+		unixtime := t.Format(time.UnixDate)
+		return timestamp{timestring, unixtime}
+	} else {
+		t, _ := time.Parse(time.UnixDate, timestring)
+		humantime := t.Format(humanForm)
+		return timestamp{humantime, timestring}
+	}
 }
