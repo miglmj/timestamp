a417ea59cc86c9c4b53a053cac3977e017b70cce
Miguel Mejia <miguelmejiamontes@me.com>
2016-04-16 13:46:28 -0400

    update dep to new github name

--- main.go
+++ main.go
@@ -2,25 +2,20 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"github.com/miguelmejiamontes/timestamp/timeutils"
-	"github.com/russross/blackfriday"
-	"io/ioutil"
+	"github.com/miglmj/timestamp/timeutils"
+	"github.com/miglmj/tools"
 	"net/http"
-	"os"
 	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", timefunc)
-	http.ListenAndServe(GetPort(), nil)
+	http.ListenAndServe(tools.GetPort(), nil)
 }
 
 func timefunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		markdown, _ := ioutil.ReadFile("README.md")
-		homepage := blackfriday.MarkdownBasic(markdown)
-		w.Write(homepage)
+		w.Write(tools.RenderHome())
 		return
 	}
 	path := strings.TrimPrefix(r.URL.Path, "/")
@@ -29,13 +24,4 @@ func timefunc(w http.ResponseWriter, r *http.Request) {
 
 	obj, _ := json.Marshal(parsedString)
 	w.Write([]byte(obj))
-}
-
-func GetPort() string {
-	var port = os.Getenv("PORT")
-	if port == "" {
-		port = "4747"
-		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
-	}
-	return ":" + port
 }
