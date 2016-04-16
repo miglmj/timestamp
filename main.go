a6e73bd8bddfca12f359ec30e3db0818d7c31975
Miguel Mejia <miguelmejiamontes@me.com>
2016-04-15 22:04:47 -0400

    move getting port to separate function

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/miguelmejiamontes/timestamp/timeutils"
 	"net/http"
 	"os"
@@ -10,7 +11,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", timefunc)
-	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	http.ListenAndServe(GetPort(), nil)
 }
 
 func timefunc(w http.ResponseWriter, r *http.Request) {
@@ -20,4 +21,14 @@ func timefunc(w http.ResponseWriter, r *http.Request) {
 
 	obj, _ := json.Marshal(parsedString)
 	w.Write([]byte(obj))
+}
+
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
